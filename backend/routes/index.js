15972b79817293369d7d0c9ec56f9b626ef2dbe4
steph <stephypadillaromero@gmail.com>
2020-11-22 11:32:45 +0100

    Routing backend enhanced with measurements

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,20 +1,21 @@
 'use strict'
 
 /*
-Conte totes les rutes, requerin al controlador (subjectCtrl o studentCtrl)
+Conte totes les rutes, requerin al controlador (measurementCtrl o resistanceCtrl)
 que es on estan implementades
  */
+
 const express = require('express')
 const resistanceCtrl = require('../controllers/resistance')
-const stationCtrl = require('../controllers/measurement')
+const measurementCtrl = require('../controllers/measurement')
 
 const api = express.Router()
 
-api.get('/getMeasurement',stationCtrl.getMeasurement),
-api.get('/getResistancesMEasurement/:measurementId',stationCtrl.getResistancesFromMeasurement),
-api.get('/afagirMostra',stationCtrl.addSampleMeasurement),
-api.get('/afagirMostra2',stationCtrl.addSampleMeasurement2),
-api.post('/addMeasurement',stationCtrl.addMeasurement),
+api.get('/getMeasurement',measurementCtrl.getMeasurement),
+api.get('/getResistancesMEasurement/:measurementId',measurementCtrl.getResistancesFromMeasurement),
+api.get('/afagirMostra',measurementCtrl.addSampleMeasurement),
+api.get('/afagirMostra2',measurementCtrl.addSampleMeasurement2),
+api.post('/addMeasurement',measurementCtrl.addMeasurement),
 api.get('/getUnnasignedResistances',resistanceCtrl.getUnnasignedResistances),
 api.get('/getResistances',resistanceCtrl.getResistances),
 api.get('/assignResistance/:measurementId/:resistanceId',resistanceCtrl.assignResistanceToMeasurement),
